72a4d587089c676bf401999bc35fe35513bbc9f6
alikaankoc1 <kocalikaan564@gmail.com>
2024-08-05 15:01:30 +0300

    car driving game
    Driving game developed using C#

---
+++ Porje_Odev/Porje_Odev/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace Porje_Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dockPanel = new System.Windows.Forms.Panel();
+            this.engel3 = new System.Windows.Forms.Panel();
+            this.engel2 = new System.Windows.Forms.Panel();
+            this.engel = new System.Windows.Forms.Panel();
+            this.panel87 = new System.Windows.Forms.Panel();
+            this.panel85 = new System.Windows.Forms.Panel();
+            this.panel84 = new System.Windows.Forms.Panel();
+            this.panel91 = new System.Windows.Forms.Panel();
+            this.panel90 = new System.Windows.Forms.Panel();
+            this.panel89 = new System.Windows.Forms.Panel();
+            this.panel88 = new System.Windows.Forms.Panel();
+            this.panel86 = new System.Windows.Forms.Panel();
+            this.carPanel = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel83 = new System.Windows.Forms.Panel();
+            this.panel82 = new System.Windows.Forms.Panel();
+            this.panel81 = new System.Windows.Forms.Panel();
+            this.panel80 = new System.Windows.Forms.Panel();
+            this.gase = new System.Windows.Forms.Panel();
+            this.brake = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.puan = new System.Windows.Forms.Label();
+            this.dockPanel.SuspendLayout();
+            this.carPanel.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dockPanel
+            // 
+            this.dockPanel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.dockPanel.Controls.Add(this.engel3);
+            this.dockPanel.Controls.Add(this.engel2);
+            this.dockPanel.Controls.Add(this.engel);
+            this.dockPanel.Controls.Add(this.panel87);
+            this.dockPanel.Controls.Add(this.panel85);
+            this.dockPanel.Controls.Add(this.panel84);
+            this.dockPanel.Controls.Add(this.panel91);
+            this.dockPanel.Controls.Add(this.panel90);
+            this.dockPanel.Controls.Add(this.panel89);
+            this.dockPanel.Controls.Add(this.panel88);
+            this.dockPanel.Controls.Add(this.panel86);
+            this.dockPanel.Controls.Add(this.carPanel);
+            this.dockPanel.Controls.Add(this.panel83);
+            this.dockPanel.Controls.Add(this.panel82);
+            this.dockPanel.Controls.Add(this.panel81);
+            this.dockPanel.Controls.Add(this.panel80);
+            this.dockPanel.Location = new System.Drawing.Point(0, 0);
+            this.dockPanel.Name = "dockPanel";
+            this.dockPanel.Size = new System.Drawing.Size(415, 684);
+            this.dockPanel.TabIndex = 0;
+            // 
+            // engel3
+            // 
+            this.engel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.engel3.Location = new System.Drawing.Point(17, 28);
+            this.engel3.Name = "engel3";
+            this.engel3.Size = new System.Drawing.Size(97, 100);
+            this.engel3.TabIndex = 13;
+            // 
+            // engel2
+            // 
+            this.engel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.engel2.Location = new System.Drawing.Point(303, 216);
+            this.engel2.Name = "engel2";
+            this.engel2.Size = new System.Drawing.Size(97, 100);
+            this.engel2.TabIndex = 12;
+            // 
+            // engel
+            // 
+            this.engel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.engel.Location = new System.Drawing.Point(156, 74);
+            this.engel.Name = "engel";
+            this.engel.Size = new System.Drawing.Size(97, 100);
+            this.engel.TabIndex = 11;
+            // 
+            // panel87
+            // 
+            this.panel87.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel87.Location = new System.Drawing.Point(275, 123);
+            this.panel87.Name = "panel87";
+            this.panel87.Size = new System.Drawing.Size(11, 78);
+            this.panel87.TabIndex = 5;
+            // 
+            // panel85
+            // 
+            this.panel85.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel85.Location = new System.Drawing.Point(120, 596);
+            this.panel85.Name = "panel85";
+            this.panel85.Size = new System.Drawing.Size(11, 78);
+            this.panel85.TabIndex = 4;
+            // 
+            // panel84
+            // 
+            this.panel84.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel84.Location = new System.Drawing.Point(120, 480);
+            this.panel84.Name = "panel84";
+            this.panel84.Size = new System.Drawing.Size(11, 78);
+            this.panel84.TabIndex = 10;
+            // 
+            // panel91
+            // 
+            this.panel91.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel91.Location = new System.Drawing.Point(275, 596);
+            this.panel91.Name = "panel91";
+            this.panel91.Size = new System.Drawing.Size(11, 78);
+            this.panel91.TabIndex = 9;
+            // 
+            // panel90
+            // 
+            this.panel90.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel90.Location = new System.Drawing.Point(275, 480);
+            this.panel90.Name = "panel90";
+            this.panel90.Size = new System.Drawing.Size(11, 78);
+            this.panel90.TabIndex = 8;
+            // 
+            // panel89
+            // 
+            this.panel89.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel89.Location = new System.Drawing.Point(275, 360);
+            this.panel89.Name = "panel89";
+            this.panel89.Size = new System.Drawing.Size(11, 78);
+            this.panel89.TabIndex = 7;
+            // 
+            // panel88
+            // 
+            this.panel88.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel88.Location = new System.Drawing.Point(275, 238);
+            this.panel88.Name = "panel88";
+            this.panel88.Size = new System.Drawing.Size(11, 78);
+            this.panel88.TabIndex = 6;
+            // 
+            // panel86
+            // 
+            this.panel86.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel86.Location = new System.Drawing.Point(275, 12);
+            this.panel86.Name = "panel86";
+            this.panel86.Size = new System.Drawing.Size(11, 78);
+            this.panel86.TabIndex = 4;
+            // 
+            // carPanel
+            // 
+            this.carPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.carPanel.Controls.Add(this.panel8);
+            this.carPanel.Controls.Add(this.panel5);
+            this.carPanel.Location = new System.Drawing.Point(17, 509);
+            this.carPanel.Name = "carPanel";
+            this.carPanel.Size = new System.Drawing.Size(97, 165);
+            this.carPanel.TabIndex = 1;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.panel10);
+            this.panel8.Controls.Add(this.panel9);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel8.Location = new System.Drawing.Point(0, 134);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(97, 31);
+            this.panel8.TabIndex = 1;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.Crimson;
+            this.panel10.Location = new System.Drawing.Point(70, 0);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(27, 31);
+            this.panel10.TabIndex = 2;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Crimson;
+            this.panel9.Location = new System.Drawing.Point(1, 0);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(27, 31);
+            this.panel9.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.panel7);
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(97, 27);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.BurlyWood;
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel7.Location = new System.Drawing.Point(67, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(30, 27);
+            this.panel7.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.BurlyWood;
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel6.Location = new System.Drawing.Point(0, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(28, 27);
+            this.panel6.TabIndex = 1;
+            // 
+            // panel83
+            // 
+            this.panel83.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel83.Location = new System.Drawing.Point(120, 360);
+            this.panel83.Name = "panel83";
+            this.panel83.Size = new System.Drawing.Size(11, 78);
+            this.panel83.TabIndex = 3;
+            // 
+            // panel82
+            // 
+            this.panel82.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel82.Location = new System.Drawing.Point(120, 242);
+            this.panel82.Name = "panel82";
+            this.panel82.Size = new System.Drawing.Size(11, 78);
+            this.panel82.TabIndex = 2;
+            // 
+            // panel81
+            // 
+            this.panel81.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel81.Location = new System.Drawing.Point(120, 123);
+            this.panel81.Name = "panel81";
+            this.panel81.Size = new System.Drawing.Size(11, 78);
+            this.panel81.TabIndex = 2;
+            // 
+            // panel80
+            // 
+            this.panel80.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel80.Location = new System.Drawing.Point(120, 12);
+            this.panel80.Name = "panel80";
+            this.panel80.Size = new System.Drawing.Size(11, 78);
+            this.panel80.TabIndex = 1;
+            // 
+            // gase
+            // 
+            this.gase.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.gase.Location = new System.Drawing.Point(448, 28);
+            this.gase.Name = "gase";
+            this.gase.Size = new System.Drawing.Size(257, 173);
+            this.gase.TabIndex = 1;
+            this.gase.MouseEnter += new System.EventHandler(this.gase_MouseEnter);
+            // 
+            // brake
+            // 
+            this.brake.BackColor = System.Drawing.Color.Crimson;
+            this.brake.Location = new System.Drawing.Point(448, 238);
+            this.brake.Name = "brake";
+            this.brake.Size = new System.Drawing.Size(257, 200);
+            this.brake.TabIndex = 2;
+            this.brake.MouseEnter += new System.EventHandler(this.brake_MouseEnter);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(750, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sol";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(750, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Sağ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(780, 277);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "SKOR";
+            // 
+            // puan
+            // 
+            this.puan.AutoSize = true;
+            this.puan.Location = new System.Drawing.Point(780, 333);
+            this.puan.Name = "puan";
+            this.puan.Size = new System.Drawing.Size(0, 16);
+            this.puan.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 686);
+            this.Controls.Add(this.puan);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.brake);
+            this.Controls.Add(this.gase);
+            this.Controls.Add(this.dockPanel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.dockPanel.ResumeLayout(false);
+            this.carPanel.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel dockPanel;
+        private System.Windows.Forms.Panel panel83;
+        private System.Windows.Forms.Panel panel82;
+        private System.Windows.Forms.Panel panel81;
+        private System.Windows.Forms.Panel panel80;
+        private System.Windows.Forms.Panel carPanel;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel gase;
+        private System.Windows.Forms.Panel brake;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel89;
+        private System.Windows.Forms.Panel panel88;
+        private System.Windows.Forms.Panel panel87;
+        private System.Windows.Forms.Panel panel86;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel85;
+        private System.Windows.Forms.Panel panel84;
+        private System.Windows.Forms.Panel panel91;
+        private System.Windows.Forms.Panel panel90;
+        private System.Windows.Forms.Panel engel;
+        private System.Windows.Forms.Panel engel2;
+        private System.Windows.Forms.Panel engel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label puan;
+    }
+}
+
---
+++ Porje_Odev/Porje_Odev/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Porje_Odev
+{
+    public partial class Form1 : Form
+    {
+        bool isGasOpen = false;
+        int sayac = 0;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            engel.Visible = false;
+         
+            engel2.Visible = false;
+            engel3.Visible = false;
+
+        }
+
+        private void gase_MouseEnter(object sender, EventArgs e)
+        {
+            isGasOpen = true;
+        }
+
+        private void brake_MouseEnter(object sender, EventArgs e)
+        {
+            isGasOpen = false;
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+
+            if (isGasOpen )
+                
+              {
+                sayac++;
+                puan.Text = sayac.ToString();
+                
+
+                if (sayac > 10)
+                {
+                    // ENGEL GÖRÜNÜRLÜK
+                    engel.Visible = true;
+                    // ENGEL KONUMU
+                    engel.Location = new Point(engel.Location.X, engel.Location.Y+1);
+
+                    if (engel.Location.Y + 75 == carPanel.Location.Y && engel.Location.X == carPanel.Location.X)  
+                    {
+                        isGasOpen = false;
+                        MessageBox.Show("Kaybettiniz. \n SKOR:" + sayac);
+                        
+                    }
+                  
+
+
+                }
+                if(sayac > 85){
+                    engel2.Visible = true;
+                    engel2.Location = new Point(engel2.Location.X, engel2.Location.Y + 1);
+                    if (engel2.Location.Y + 75 == carPanel.Location.Y && engel2.Location.X == carPanel.Location.X)
+                    {
+                        isGasOpen = false;
+                        MessageBox.Show("Kaybettiniz. \n SKOR:" + sayac);
+                        
+                    }
+
+                }
+                if (sayac > 117)
+                {
+                    engel3.Visible = true;
+                    engel3.Location = new Point(engel3.Location.X, engel3.Location.Y + 1);
+                    if (engel3.Location.Y + 75 == carPanel.Location.Y && engel3.Location.X == carPanel.Location.X)
+                    {
+                        isGasOpen = false;
+                        MessageBox.Show("Kaybettiniz. \n SKOR:" +  sayac );
+                        
+                        
+                    }
+
+                }
+
+
+
+                // yolda olan şeritlerin hareketini sağlayan kodlar
+
+                panel80.Location = new Point(panel80.Location.X, panel80.Location.Y + 1);
+                  panel81.Location = new Point(panel81.Location.X, panel81.Location.Y + 1);
+                  panel82.Location = new Point(panel82.Location.X, panel82.Location.Y + 1);  
+                  panel83.Location = new Point(panel83.Location.X, panel83.Location.Y + 1);
+                  panel84.Location = new Point(panel84.Location.X, panel84.Location.Y + 1);
+                  panel85.Location = new Point(panel85.Location.X, panel85.Location.Y + 1);
+                  panel86.Location = new Point(panel86.Location.X, panel86.Location.Y + 1);
+                  panel87.Location = new Point(panel87.Location.X, panel87.Location.Y + 1);
+                  panel88.Location = new Point(panel88.Location.X, panel88.Location.Y + 1);
+                  panel89.Location = new Point(panel89.Location.X, panel89.Location.Y + 1);
+                  panel90.Location = new Point(panel90.Location.X, panel90.Location.Y + 1);
+                  panel91.Location = new Point(panel91.Location.X, panel91.Location.Y + 1);
+
+                  if(panel85.Location.Y > 504)
+                  {
+                      panel85.Location = new Point(panel85.Location.X, 484);
+                      panel91.Location = new Point(panel91.Location.X, 484 );
+                  }
+
+                  if (panel84.Location.Y > 410)
+                  {
+                      panel84.Location = new Point(panel84.Location.X, 390);
+                      panel90.Location = new Point(panel90.Location.X, 390 );
+                  }
+                  if (panel83.Location.Y > 312)
+                  {
+                      panel83.Location = new Point(panel83.Location.X, 292);
+                      panel89.Location = new Point(panel89.Location.X, 292 );
+                  }
+                  if (panel82.Location.Y > 217)
+                  {
+                      panel82.Location = new Point(panel82.Location.X, 197 );
+                      panel88.Location = new Point(panel88.Location.X, 197);
+                  }
+                  if (panel81.Location.Y > 120)
+                  {
+                      panel81.Location = new Point(panel81.Location.X, 100);
+                      panel87.Location = new Point(panel87.Location.X, 100 );
+                  }
+                  if (panel80.Location.Y > 30)
+                  {
+                      panel80.Location = new Point(panel80.Location.X, 10);
+                      panel86.Location = new Point(panel86.Location.X, 10);
+
+                  }
+
+
+
+
+              } 
+
+            
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        // sağ sol tuş butonlar 
+        private void button1_Click(object sender, EventArgs e)
+        {
+          if(carPanel.Location.X == engel.Location.X)
+            {
+                carPanel.Location = new Point(engel3.Location.X, carPanel.Location.Y);
+            }
+          if(carPanel.Location.X == engel2.Location.X)
+            {
+                carPanel.Location = new Point(engel.Location.X, carPanel.Location.Y);
+            }
+          
+       
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           if(carPanel.Location.X == engel.Location.X)
+            {
+                carPanel.Location = new Point(engel2.Location.X, carPanel.Location.Y);
+            }
+            if (carPanel.Location.X == engel3.Location.X)
+            {
+                carPanel.Location = new Point(engel.Location.X, carPanel.Location.Y);
+            }
+
+        }
+
+    }
+}
